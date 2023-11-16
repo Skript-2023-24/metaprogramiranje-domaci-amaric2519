19178d9223086ac91708bc3659d1d58106f266f6
amaric2519 <62028665+amaric2519@users.noreply.github.com>
2023-11-16 17:15:18 +0100

    Delete connect.rb

--- connect.rb
+++
@@ -1,8 +0,0 @@
-require 'bundler'
-Bundler.require
-
-
-def connect(file_name = '')
-  session = GoogleDrive::Session.from_service_account_key('client_secret.json')
-  session.spreadsheet_by_key('1ujIZJtVUSXs5RDJ_ZzUPPqc5BbOE4ZMyoG1URg_g38U')
-end
